80f1478a5b080df46397f252d787f36c7c70b6ca
Felipe Bento Xavier <52262336+felipexavier1995@users.noreply.github.com>
2026-06-14 11:52:40 -0300

    Update comandos.sql

--- comandos.sql
+++ comandos.sql
@@ -1,6 +1,16 @@
 -- Comandos usados dentro do postgresql.
 
+--- Para entrar no postgres com o docker.
+docker exec -it meu-postgres psql -U admin -d meubanco
+
 -- Visualizar todos os banco de dados.
 \l
 
---
+-- Visualizar todos os usuários.
+\du
+
+-- Visualizar todas as tabelas do banco de dados
+\dt
+
+-- Visualizar em qual banco de dados está conectado.
+\conninfo
